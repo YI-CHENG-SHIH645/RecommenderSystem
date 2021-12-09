5f9d9cd2a373e09e0496c8db9a7b235597425d2e
YI-CHENG-SHIH645 <z2x98653322@gmail.com>
2021-12-09 16:44:09 +0800

    add files

--- main.cpp
+++ main.cpp
@@ -5,20 +5,5 @@ int main() {
     CF cf("../data/Book_reviews/Book_reviews/BX-Book-Ratings.csv");
     auto items = cf.recommended_items_for_user(54884);
     auto users = cf.recommended_users_for_item(88740);
-//    cf.test_rmse("user-based");
-
-//    SP_ROW mat(6, 8);
-//    mat.coeffRef(0, 0) = 3;
-//    mat.coeffRef(5, 5) = 4;
-//    mat.coeffRef(5, 7) = 4;
-//    SP_ROW mat_block = mat.bottomRightCorner(3, 4);
-//    SV row = mat_block.row(2);
-//    std::cout << row << std::endl;
-//    for(int i=0; i<row.nonZeros(); ++i) {
-//        std::cout << *(row.innerIndexPtr()+i) << std::endl;
-//    }
-//    SV row = mat.row(5);
-//    for(int i=0; i<row.nonZeros(); ++i) {
-//        mat.coeffRef(5, *(row.innerIndexPtr() + i)) = 0;
-//    }
+    cf.test_rmse("user-based");
 }
